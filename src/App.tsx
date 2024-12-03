7578dd8f581e92b34e8a89b8a16c9ac95c168c8f
VacBal <vaczi.balazs.zsolt@gmail.com>
2024-12-03 22:08:35 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { GlobalProvider } from "./assets/context/GlobalContext";
 import HomePage from "./assets/pages/HomePage";
 import LoginPage from "./assets/pages/LoginPage";
 import RegisterPage from "./assets/pages/RegisterPage";
-import ProfilePage from "./assets/pages/ProfilePage";
-import ProductListPage from "./assets/pages/ProductListPage";
+import NavBar from "./assets/pages/NavBar";
 
 const App: React.FC = () => {
   return (
     <GlobalProvider>
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/products" element={<ProductListPage />} />
         </Routes>
       </Router>
     </GlobalProvider>
